6cdf19e03b83513f693601968c869e0f62309ea1
NARESH <64158150+NARESH-JPG@users.noreply.github.com>
2021-10-14 13:36:44 +0530

    Day_1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Basic_Construct_For_Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("What is Your fav Snack\n");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many Plates You can have it at a time?\n");
+            //Coverting bydefault value in string into int
+            int count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(name);
+            Console.WriteLine(count);
+            if (count >= 2)
+            {
+
+                Console.WriteLine("Too much of snacking is not a healthy sign..!!");
+            }
+
+        }
+    }
+}
